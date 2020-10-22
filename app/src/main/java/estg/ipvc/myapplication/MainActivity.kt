90847be172d55dff2b909ea77ce4a852858caa03
brunogabriel997 <brunogabriel997@gmail.com>
2020-10-22 10:46:42 +0100

    Multilanguage finished

--- app/src/main/java/estg/ipvc/myapplication/MainActivity.kt
+++ app/src/main/java/estg/ipvc/myapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package estg.ipvc.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("***TAG", "onCreate")
+        Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("***TAG","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("***TAG", "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("***TAG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("***TAG","onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
